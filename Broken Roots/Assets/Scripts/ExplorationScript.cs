df8cb2af39981c5bf7238cfa3f6dbcce766f1d28
Alex Carpentieri <alexengineering1027@gmail.com>
2019-01-26 14:55:47 -0700

    Almost works. need to get collision detection

--- Broken Roots/Assets/Scripts/ExplorationScript.cs
+++ Broken Roots/Assets/Scripts/ExplorationScript.cs
@@ -7,10 +7,14 @@ public class ExplorationScript : MonoBehaviour
     [Header("Show In Inspector")]
 
     GameObject playerObject;
+    GameObject randomPrefabChoice;
+    GameObject gameManager;
+    public GameObject fogObject;
     // Start is called before the first frame update
     void Start()
     {
         playerObject = GameObject.Find("Player");
+        gameManager = GameObject.Find("ExplorationGameState");
     }
 
     // Update is called once per frame
@@ -19,23 +23,53 @@ public class ExplorationScript : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.name == "UpwardExplore")
         {
-
+            tryToMakeTile(fogObject, new Vector3(0, 0, 200));
+            tryToMakeTile(fogObject, new Vector3(-100, 0, 200));
+            tryToMakeTile(fogObject, new Vector3(-200, 0, 200));
+            tryToMakeTile(fogObject, new Vector3(100, 0, 200));
+            tryToMakeTile(fogObject, new Vector3(200, 0, 200));
+            Debug.Log("UpwardExplore collided");
         }
         else if (collision.gameObject.name == "DownwardExplore")
         {
-
+            tryToMakeTile(fogObject, new Vector3(0, 0, -200));
+            tryToMakeTile(fogObject, new Vector3(-100, 0, -200));
+            tryToMakeTile(fogObject, new Vector3(-200, 0, -200));
+            tryToMakeTile(fogObject, new Vector3(100, 0, -200));
+            tryToMakeTile(fogObject, new Vector3(200, 0, -200));
+            Debug.Log("DownExplore collided");
         }
         else if (collision.gameObject.name == "LeftExplore")
         {
-
+            tryToMakeTile(fogObject, new Vector3(-200, 0, 0));
+            tryToMakeTile(fogObject, new Vector3(-200, 0, -100));
+            tryToMakeTile(fogObject, new Vector3(-200, 0, -200));
+            tryToMakeTile(fogObject, new Vector3(-200, 0, 100));
+            tryToMakeTile(fogObject, new Vector3(-200, 0, 200));
+            Debug.Log("LeftExplore collided");
         }
         else if (collision.gameObject.name == "RightExplore")
         {
+            tryToMakeTile(fogObject, new Vector3(200, 0, 0));
+            tryToMakeTile(fogObject, new Vector3(200, 0, -100));
+            tryToMakeTile(fogObject, new Vector3(200, 0, -200));
+            tryToMakeTile(fogObject, new Vector3(200, 0, 100));
+            tryToMakeTile(fogObject, new Vector3(200, 0, 200));
+            Debug.Log("RightExplore collided");
+        }
+    }
 
+    void tryToMakeTile(GameObject tileToSpawn, Vector3 newTileLocation)
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(newTileLocation, 5);
+        if (!(Physics.CheckSphere(newTileLocation, 10)))
+        {
+            GameObject newTileInstance = GameObject.Instantiate(tileToSpawn, newTileLocation, Quaternion.identity);
         }
     }
 }

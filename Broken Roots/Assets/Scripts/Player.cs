db7ab71a4b3128321844a0d994e24202300e1662
JOSHUA ROSEN <u0996127@ad.utah.edu>
2019-01-26 14:09:02 -0700

    PLAYER SCRIPT MOVE with rigidbody

---
+++ Broken Roots/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private new Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+    }
+}

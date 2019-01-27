9b12e6ba407b63fd87362d6f598467616375ce5e
Unknown <jsengineer@yahoo.com>
2019-01-27 02:06:12 -0700

    Fixed broken menu

--- Broken Roots/Assets/Scripts/MenuController.cs
+++ Broken Roots/Assets/Scripts/MenuController.cs
@@ -2,60 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MenuController : MonoBehaviour
 {
     Resolution[] resolutions;
     public Dropdown resolutionDropdown;
-
-    private void Start()
-    {
-        resolutions = Screen.resolutions;
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResIndex = 0;
-
-        for(int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if(resolutions[i].width == Screen.currentResolution.width 
-                && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
-    public void SetResolution (int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
-
-    public void NewGame()
-    {
-        SceneManager.LoadScene("Scenes/Engineer/Alex");
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Game Quit");
-        Application.Quit();
-    }
-}
-
-    Resolution[] resolutions;
-    public Dropdown resolutionDropdown;
+    public AudioMixer audioMixer;
 
     public void optionsMenu()
     {
@@ -91,4 +45,19 @@ public class MenuController : MonoBehaviour
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
     public void NewGame()
+    {
+        SceneManager.LoadScene("Scenes/Engineer/Alex");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+}

1f07c6cc8ddd0c628a28eba45d91d681eac87308
Tyler <tyler.trombley.15@gmail.com>
2019-01-27 00:21:17 -0700

    Menu Progress

---
+++ Broken Roots/Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+   public void NewGame()
+    {
+        SceneManager.LoadScene("Scenes/Engineer/Alex");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+}

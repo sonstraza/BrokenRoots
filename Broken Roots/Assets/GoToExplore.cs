3295fa23c233a08dec70b89f3b984f8326589f6a
Thomas <thomoh777@gmail.com>
2019-01-27 01:57:16 -0700

    Town stuff
    Added town scene that can move to explore scene. Need to be able to go back to town from explore. Art needs to be added to town scene, but skeleton code is there. Need to add list of found npcs to townMaker and Data.

---
+++ Broken Roots/Assets/GoToExplore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToExplore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider ChangeScene)
+    {
+        if (ChangeScene.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Alex");
+        }
+    }
+}
---
+++ Broken Roots/Assets/Scripts/townMaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class townMaker : MonoBehaviour
+{
+    public const int townSize = 6;
+    public GameObject[] NPCTile = new GameObject[townSize];
+    public List<GameObject> npcsInTown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject npc in npcsInTown)
+        {
+            if(npc.name == "Botanist_GRP")
+            {
+                //NPCTile[0]
+            }
+            if(npc.name == "Doctor_GRP")
+            {
+                //NPCTile[1]
+            }
+            if(npc.name == "Farmer_GRP")
+            {
+                //NPCTile[2]
+            }
+            if(npc.name == "Merchant_GRP")
+            {
+                //NPCTile[3]
+            }
+        }
+        //Implement home and exit tile stuff
+        //NPCTile[4] Add HomeTile stuff
+        //NPCTile[5] Add ExitTile stuff
+    }
+}

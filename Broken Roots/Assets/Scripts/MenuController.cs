0d718c83f22a10cc455660d076578024f2b9c524
Unknown <jsengineer@yahoo.com>
2019-01-27 01:35:46 -0700

    Working options menu

--- Broken Roots/Assets/Scripts/MenuController.cs
+++ Broken Roots/Assets/Scripts/MenuController.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-   public void NewGame()
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResIndex = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width 
+                && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void NewGame()
     {
         SceneManager.LoadScene("Scenes/Engineer/Alex");
     }
@@ -15,4 +54,12 @@ public class MenuController : MonoBehaviour
         Debug.Log("Game Quit");
         Application.Quit();
     }
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
 }

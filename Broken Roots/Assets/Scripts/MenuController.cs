65e18e60f961a218edfa1382fbd24dc218b999ec
Unknown <jsengineer@yahoo.com>
2019-01-27 01:52:14 -0700

    Revert "Working options menu"
    This reverts commit 0d718c83f22a10cc455660d076578024f2b9c524.

--- Broken Roots/Assets/Scripts/MenuController.cs
+++ Broken Roots/Assets/Scripts/MenuController.cs
@@ -2,11 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
-using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResIndex = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width 
+                && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Scenes/Engineer/Alex");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+}
+
     Resolution[] resolutions;
     public Dropdown resolutionDropdown;
 
@@ -45,21 +92,3 @@ public class MenuController : MonoBehaviour
     }
 
     public void NewGame()
-    {
-        SceneManager.LoadScene("Scenes/Engineer/Alex");
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Game Quit");
-        Application.Quit();
-    }
-
-    public AudioMixer audioMixer;
-
-    public void SetVolume (float volume)
-    {
-        audioMixer.SetFloat("volume", volume);
-    }
-
-}

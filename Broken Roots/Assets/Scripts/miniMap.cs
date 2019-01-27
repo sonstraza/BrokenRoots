d719eb84cf1bb482fd1db5e1693a9f5a4d07cade
Alex Carpentieri <alexengineering1027@gmail.com>
2019-01-27 11:26:27 -0700

    can move without freeze

--- Broken Roots/Assets/Scripts/miniMap.cs
+++ Broken Roots/Assets/Scripts/miniMap.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class miniMap : MonoBehaviour
 {
-    bool miniMapOn = false;
     GameObject miniMapCam;
     // Start is called before the first frame update
     void Start()
